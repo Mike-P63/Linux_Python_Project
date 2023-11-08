7dc0407ed33c7fba4ce3f261fe5013120a2d05ba
Mike-P63 <rumaish@rambler.ru>
2023-11-08 08:41:30 +0300

    Added changes to hw 2 files

--- test_positive.py
+++ test_positive.py
@@ -4,6 +4,7 @@ tst = "/home/user/tst"
 out = "/home/user/out"
 folder1 = "/home/user/folder1"
 folder2 = "/home/user/folder2"
+folder_extract = "/home/user/folder_extract"
 
 
 def checkout(cmd, text):
@@ -35,15 +36,14 @@ def test_step4():
     result1 = checkout("cd {}; 7z u {}/arx2.7z".format(tst, out), "Everything is Ok"), "test4 FAIL"
 
 
-def test_step5():
-    result1 = checkout("cd {}; 7z d arx2.7z".format(out), "Everything is Ok"), "test5 FAIL"
+# def test_step5():
+#     result1 = checkout("cd {}; 7z d arx2.7z".format(out), "Everything is Ok"), "test5 FAIL"
 
 
 def test_step6():
-    result1 = checkout("cd {}; 7z l arx2.7z".format(out), "2 files"), "test6 FAIL"
-    result2 = checkout("cd {}; 7z l arx2.7z".format(out), "qwe"), "test6 FAIL"
-    result3 = checkout("cd {}; 7z l arx2.7z".format(out), "rty"), "test6 FAIL"
-
+    result1 = checkout("cd {}; 7z l arx2.7z".format(out, folder_extract), "qwe")
+    result2 = checkout("cd {}; 7z l arx2.7z".format(out, folder_extract), "rty")
+    assert result1 and result2, "test6 FAIL"
 
 def test_step7():
     result1 = checkout("cd {}; 7z x arx2.7z -o{} -y".format(out, folder2), "Everything is Ok")
@@ -56,4 +56,5 @@ def test_8():
     result = subprocess.run("crc32 /home/user/out/arx2.7z", shell=True, stdout=subprocess.PIPE, encoding="utf-8")
     data = result.stdout.rstrip().upper()
     assert checkout(f"cd {out}; 7z h arx2.7z", data), "test8 FAIL"
+
 
